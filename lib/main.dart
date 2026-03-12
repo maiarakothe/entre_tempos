902b212244625f176017bf5a016525ee3b9cca06
Maiara Kothe <maiarakothe0@gmail.com>
2026-03-12 15:44:46 -0300

    feat: adiciona splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:entre_tempos/ui/pages/login_page.dart';
+import 'package:entre_tempos/ui/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: const LoginPage(),
+      home: const SplashPage(),
       debugShowCheckedModeBanner: false,
       locale: const Locale('pt', 'BR'),
       supportedLocales: const <Locale>[Locale('pt', 'BR')],
---
+++ lib/ui/pages/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../core/Colors.dart';
+import 'letter_page.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+    Future<dynamic>.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute<dynamic>(builder: (_) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: DefaultColors.colorTest),
+        child: Center(
+          child: FadeTransition(
+            opacity: controller,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                Icon(Icons.mail_outline, size: 90, color: Colors.white),
+                SizedBox(height: 20),
+                Text(
+                  "EntreTempos",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
